56740c941c5c1ddd35968119e8d9fba3cada2910
Jacob Simmons <jakerstx@gmail.com>
2022-09-18 00:39:43 -0500

    created getComputerChoice() function which returns a random rock paper or scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,14 @@
-console.log("hello world irs me again");
+function getComputerChoice(){
+    num = Math.floor(Math.random()*3);
+    if(num == 0){
+        value = "Rock";
+    }
+    else if(num == 1){
+        value = "Paper";
+    }
+    else{
+        value = "Scissors";
+    }
+    return value;
+}
+console.log(getComputerChoice());
