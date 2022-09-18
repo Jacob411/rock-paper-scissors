0760955e8e5924f8571be28c9c603a41dfecdab9
Jacob Simmons <jakerstx@gmail.com>
2022-09-18 01:13:40 -0500

    create playRound() function that plays a round of rock paper scissors.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -11,4 +11,28 @@ function getComputerChoice(){
     }
     return value;
 }
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if(playerSelection ==  computerSelection){
+        output = "Its a tie!\n";
+    }
+    else if((playerSelection == "rock" & computerSelection == "paper") || (playerSelection == "paper" & computerSelection == "scissors") || (playerSelection == "scissors" & computerSelection == "rock")){
+        output = "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+    else {
+        output = "You won! " + playerSelection + " beats " + computerSelection;
+    }
+    return output;
+}
+function game(){
+    for(let i = 0; i < 5; i++){
+        playerSelection = prompt("Choose rock, paper, or scissors", "rock");
+        playRound
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log("player: " + playerSelection + "\tcomputer: " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
